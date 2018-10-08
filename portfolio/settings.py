ffe63af9bcb3195cafdbb0c96f58bba5604b0553
Rimma <ssoffitt@qip.ru>
2018-10-08 21:45:53 +0300

    fix allowed_hosts

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,8 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 #DEBUG = True
 DEBUG=config('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = [vast-shore-40928.herokuapp.com]
-
+ALLOWED_HOSTS = ['vast-shore-40928.herokuapp.com']
 
 # Application definition
 
